fceb8c454601549467c19d93a436e0d3b56fad2f
EyesAblaze <136992337+EyesAblaze@users.noreply.github.com>
2024-04-26 22:16:49 +0100

    remove comment

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,6 @@
  * @type {import('gatsby').GatsbyConfig}
  */
 module.exports = {
-  //let me add a comment here
   siteMetadata: {
     title: `Saturday Sweep`,
     siteUrl: `https://www.yourdomain.tld`,
