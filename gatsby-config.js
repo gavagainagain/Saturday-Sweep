55f717bf0d15b7334179f4ba546f55b48f68f3c0
EyesAblaze <136992337+EyesAblaze@users.noreply.github.com>
2024-04-26 21:16:16 +0100

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Saturday Sweep`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
