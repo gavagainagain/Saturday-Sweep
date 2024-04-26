1da88f0f30da27cbe35c0074e4b84912b10b8bab
EyesAblaze <136992337+EyesAblaze@users.noreply.github.com>
2024-04-26 21:50:56 +0100

    Simple comment change

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,6 +2,7 @@
  * @type {import('gatsby').GatsbyConfig}
  */
 module.exports = {
+  //let me add a comment here
   siteMetadata: {
     title: `Saturday Sweep`,
     siteUrl: `https://www.yourdomain.tld`,
