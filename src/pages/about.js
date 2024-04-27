ef3a312427aeeceeb7e3bde0dad85d67e7f00e23
EyesAblaze <136992337+EyesAblaze@users.noreply.github.com>
2024-04-27 18:22:52 +0100

    Completion of checkpoint 2

---
+++ src/pages/about.js
@@ -0,0 +1,21 @@
+import * as React from 'react'
+import Layout from '../components/layout'
+
+const AboutPage = () => {
+  return (
+    <main>
+        <Layout pageTitle="About Me">
+            <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
+        </Layout>
+    </main>
+  )
+}
+
+export const Head = () => (
+    <>
+      <title>About Me</title>
+      <meta name="description" content="This is a personal project that aims to help me understand how react works" />
+    </>
+  )
+
+  export default AboutPage
